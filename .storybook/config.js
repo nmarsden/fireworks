936ed535578f8aaca28a11e62f3ae3215c76cc74
Neil Marsden <nmarsden@aconex.com>
2019-10-30 22:19:59 +1100

    tweaked discarded styles and now using a dark theme for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
-import { configure } from '@storybook/angular';
+import { configure, addParameters } from '@storybook/angular';
+import { themes } from '@storybook/theming';
+
+addParameters({
+  options: {
+    theme: themes.dark,
+  },
+});
 
 // automatically import all files ending in *.stories.ts
 configure(require.context('../src/stories', true, /\.stories\.ts$/), module);
