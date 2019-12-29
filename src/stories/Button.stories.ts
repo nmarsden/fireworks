d967db1385eee170644a60a70217b54fc24561b5
Neil Marsden <nmarsden@aconex.com>
2019-12-29 11:43:23 +1100

    added a travelling border animation to buttons & restyled colour buttons
    - only show the button animation when the button is inside an open modal
    - using :host-context(.app-modal-open) .button-anim { display: block; }
    - this is intended to reset the animation to prevent multiple button animations being out of sync
    
    - also added a slight box-shadow to buttons to give them some depth

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,36 +1,54 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
-import { ButtonComponent } from '../app/core/button/button.component';
+import { CoreModule } from '../app/core/core.module';
 
 storiesOf('Button', module)
   .addDecorator(
     moduleMetadata({
-      declarations: [
-        ButtonComponent
-      ],
-      imports: []
+      declarations: [],
+      imports: [ CoreModule ]
     })
   )
   .add('default', () => {
     return {
-      template: `<div style="display:flex; width:100%; height:100vh; justify-content:center; align-items:center;">
+      template: `<div style="display:flex; width:100%; height:100vh; flex-direction:column; align-items:center;">
                    <app-button>Button</app-button>
+                   <app-modal id="quit-game-modal" [isOpen]="true" [position]="'rock-bottom'">
+                     <div class="modal-body">
+                       <div class="modal-heading">Modal</div>
+                       <app-button>Button</app-button>
+                     </div>
+                   </app-modal>
                  </div>`,
     };
   })
   .add('no padding', () => {
     return {
-      template: `<div style="display:flex; width:100%; height:100vh; justify-content:center; align-items:center;">
+      template: `<div style="display:flex; width:100%; height:100vh; flex-direction:column; align-items:center;">
                    <app-button [noPadding]="true">Button</app-button>
+                   <app-modal id="quit-game-modal" [isOpen]="true" [position]="'rock-bottom'">
+                     <div class="modal-body">
+                       <div class="modal-heading">Modal</div>
+                       <app-button [noPadding]="true">Button</app-button>
+                     </div>
+                   </app-modal>
                  </div>`,
     };
   })
   .add('colours', () => {
     const colours = [ 'white', 'red', 'yellow', 'green', 'blue' ];
     return {
-      template: `<div style="display:flex; width:100%; height:100vh; justify-content:center; align-items:center;">
+      template: `<div style="display:flex; width:100%; height:100vh; flex-direction:column; align-items:center;">
                    <div class="button-container">
                      <app-button *ngFor="let colour of colours" [colour]="colour"></app-button>
                    </div>
+                   <app-modal id="quit-game-modal" [isOpen]="true" [position]="'rock-bottom'">
+                     <div class="modal-body">
+                       <div class="modal-heading">Modal</div>
+                       <div class="button-container">
+                         <app-button *ngFor="let colour of colours" [colour]="colour"></app-button>
+                       </div>
+                     </div>
+                   </app-modal>
                  </div>`,
       props: {
         colours
